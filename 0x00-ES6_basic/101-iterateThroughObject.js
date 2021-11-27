cdabba3d6525636f8b77dbc95ff99777a1977841
KibiBC <69991794+Koderua@users.noreply.github.com>
2021-11-27 15:27:24 +0300

    Delete 0x00-ES6_basic directory

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++
@@ -1,8 +0,0 @@
-export default function iterateThroughObject(reportWithIterator) {
-  let employeesName = '';
-  reportWithIterator.forEach((name, index) => {
-    employeesName += name;
-    if (index !== reportWithIterator.length - 1) employeesName += ' | ';
-  });
-  return employeesName;
-}
